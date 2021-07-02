6d1b9254feb1da2cbf915cefc81b552207cab1dc
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-07-02 17:18:49 -0400

    check question index and render next question

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-// click the start button to begin quiz
-$("#start").click(function() {
-  $("#begin-quiz").addClass("hide");
-  $("#questions").removeClass("hide");
-  getQuestion();
-})
+// // click the start button to begin quiz
+// $("#start").click(function() {
+//   $("#begin-quiz").addClass("hide");
+//   $("#questions").removeClass("hide");
+//   getQuestion();
+// })
 
 // array of objects to use for quiz questions.
 const questionBank = [
@@ -41,124 +41,108 @@ const questionBank = [
   },
 ]
 
+const startBox = document.getElementById("begin-quiz");
+const questionBox = document.getElementById("questions");
+const initialsBox = document.getElementById("all-done")
+const highScoresBox = document.getElementById("highscores")
+
+
+// returns the id of the button clicked
+const btns = document.querySelectorAll('button')
+
+btns.forEach(function(btn) {
+  btn.addEventListener('click', btnClick)
+})
+
+function btnClick(e) {
+  let button = (e.target.getAttribute('id'))
+  
+  if (button === 'start') {
+    startBox.setAttribute("class", "hide");
+    questionBox.setAttribute("class", "show");
+    renderQuestion();  
+
+  } else if (button === 'submit-initials') {
+    // run getScore function, post scores from local storage
+    initialsBox.setAttribute("class", "hide");
+    highScoresBox.setAttribute("class", "show");
+
+  } else if (button === 'go-back') {
+    highScoresBox.setAttribute("class", "hide");
+    startBox.setAttribute("class", "show");
+    // do not clear local storage
+  } else if (button === 'clear-scores') {
+    console.log('clear high scores');
+    // clear local storage, remain on page
+  }
+}
+
+
+// renders question and answer choices to screen
+const getQuestionText = document.getElementById('question-text');
+const answer1 = document.getElementById('A');
+const answer2 = document.getElementById('B');
+const answer3 = document.getElementById('C');
+const answer4 = document.getElementById('D');
+
+// Variables to use in the loop to select new questions.
+const lastQuestion = questionBank.length;
 let currentQuestion = 0;
-let lastQuestion = questionBank.length - 1;
+let correctAnswer = questionBank[currentQuestion].correct;
+
+const answerA = questionBank[1];
+const answerB = questionBank[2];
+const answerC = questionBank[3];
+const answerD = questionBank[4];
 
 
-let getQuestion = function() {
+let renderQuestion = function() {
 
+  getQuestionText.textContent = questionBank[currentQuestion].question;
+  answer1.innerText = questionBank[currentQuestion].answerA;
+  answer2.innerText = questionBank[currentQuestion].answerB;
+  answer3.innerText = questionBank[currentQuestion].answerC;
+  answer4.innerText = questionBank[currentQuestion].answerD;
+}
 
+const answerBtns = document.querySelectorAll('.answers')
 
+answerBtns.forEach(function(ans) {
+  ans.addEventListener('click', checkAnswer)
+})
 
+const answerCorrect = document.getElementById("correct");
 
+function checkAnswer(e) {
+  console.log(currentQuestion)
 
+  let answer = (e.target.getAttribute('id'))
 
-
-
-
-
-
-
-
-
-// const startBox = document.getElementById("begin-quiz");
-// const questionBox = document.getElementById("questions");
-// const initialsBox = document.getElementById("all-done")
-// const highScoresBox = document.getElementById("highscores")
-
-
-// // this will return the id of the button clicked
-// const btns = document.querySelectorAll('button')
-
-// btns.forEach(function(btn) {
-//   btn.addEventListener('click', btnClick)
-// })
-
-// function btnClick(e) {
-//   let button = (e.target.getAttribute('id'))
+  if (answer !== correctAnswer) {
+    answerCorrect.setAttribute('class', 'show');
+    answerCorrect.textContent= "Incorrect.";
+  // run score funtion}
+    setTimeout(function() {
+    answerCorrect.setAttribute('class', 'hide');
+    }, 2000);
   
-//   if (button === 'start') {
-//     startBox.setAttribute("class", "hide");
-//     questionBox.setAttribute("class", "show");
-//     nextQuestion();  
+    } else {
+    answerCorrect.setAttribute('class', 'show');
+    answerCorrect.textContent= "Correct!";
+    // update score
 
-//   } else if (button === 'submit-initials') {
-//     // run getScore function, post scores from local storage
-//     initialsBox.setAttribute("class", "hide");
-//     highScoresBox.setAttribute("class", "show");
+    setTimeout(function() {
+    answerCorrect.setAttribute('class', 'hide');
+    }, 2000);
+  }
 
-//   } else if (button === 'go-back') {
-//     highScoresBox.setAttribute("class", "hide");
-//     startBox.setAttribute("class", "show");
-//     // do not clear local storage
-//   } else if (button === 'clear-scores') {
-//     console.log('clear high scores');
-//     // clear local storage, remain on page
-//   }
-// }
+  currentQuestion++;
 
-
-// // renders question and answer choices to screen
-// const getQuestionText = document.getElementById('question-text');
-// const answer1 = document.getElementById('A');
-// const answer2 = document.getElementById('B');
-// const answer3 = document.getElementById('C');
-// const answer4 = document.getElementById('D');
-
-// // Variables to use in the loop to select new questions.
-// const lastQuestion = questionBank.length - 1;
-// let currentQuestion = 0;
-// let selectQuestion = questionBank[currentQuestion];
-// let correctAnswer = questionBank[currentQuestion].correct;
-
-// const answerA = questionBank[1];
-// const answerB = questionBank[2];
-// const answerC = questionBank[3];
-// const answerD = questionBank[4];
-
-
-// // check if last question has appeared and either run renderQquestion or end quiz and go to the score and initials screen
-// let nextQuestion = function() {
-//   if (currentQuestion > lastQuestion) {
-//     questionBox.setAttribute("class", "hide");
-//     initialsBox.setAttribute("class", "show");
-//     // run score funtion
-
-//   } else {
-//     renderQuestion()
-//   }
-// }
-
-// let renderQuestion = function() {
-//       getQuestionText.textContent = selectQuestion.question;
-//       answer1.innerText += selectQuestion.answerA;
-//       answer2.innerText += selectQuestion.answerB;
-//       answer3.innerText += selectQuestion.answerC;
-//       answer4.innerText += selectQuestion.answerD;
-// }
-
-// const answerBtns = document.querySelectorAll('.answers')
-
-// answerBtns.forEach(function(ans) {
-//   ans.addEventListener('click', checkAnswer)
-// })
-
-// const answerCorrect = document.getElementById("correct");
-
-// function checkAnswer(e) {
-//   let answer = (e.target.getAttribute('id'))
-  
-//   if (answer === correctAnswer) {
-//     answerCorrect.setAttribute('class', 'show');
-//     answerCorrect.textContent= "Correct!";
-//     // show for 2 seconds, then run nextQuestion
-//   } else {
-//     console.log("Incorrect");
-//     answerCorrect.setAttribute('class', 'show');
-//     answerCorrect.textContent= "Incorrect.";
-//     //show "incorrect" for 2 seconds, run nextQuestion
-//   }
-// }
-
-
-
+  if (currentQuestion < lastQuestion) {
+    console.log(currentQuestion);
+    renderQuestion(currentQuestion);
+  } else {
+    questionBox.setAttribute("class", "hide");
+    initialsBox.setAttribute("class", "show");
+  }
+}
